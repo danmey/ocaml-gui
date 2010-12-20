9b4eb47a2cf85c4c51cba221c276adde5d0884ea
Wojciech Meyer <wojciech.meyer@gmail.com>
2010-12-20 00:23:41 +0000

    Command line arguments. Project.

--- example.ml
+++ example.ml
@@ -55,10 +55,11 @@ open BatFloat
 (*       () *)
 (* end *)
 
+let project_name = ref "canvas.txt"
 
 open Blocks
 
-let texture_generator_view () =
+let texture_generator_view() =
   let kont = ref None in
   let open Texgen in
       let control_pane = new texture_preview(* frame (Layout.fixed_vertical 5 25) *) in
@@ -144,69 +145,33 @@ let texture_generator_view () =
       let main_frame = new frame (Layout.vertical_fixed ~sizes:[25]) in
       main_frame#add ((menu_bar 
                          [Entry ("Save", 
-                                 (fun () -> graphical_pane # write "canvas.txt"));
+                                 (fun () -> graphical_pane # write !project_name));
                           Entry ("Open",
-                                 (fun () -> graphical_pane # read "canvas.txt"));
+                                 (fun () -> graphical_pane # read !project_name));
                          ]) :> draggable);
       main_frame#add (main_view :> draggable);
+      graphical_pane # read !project_name;
       main_frame
-      
-        
-  
 
 
-      
+let commandline_spec =
+  let open Arg in
+  let open BatArg in
+  [ command ~doc:"Filename of a project to work with. Default project is \"canvas.txt\"."
+    "-project"
+    (Set_string project_name)]
 
-(* let a () = *)
-(*   Gui.init *)
-(*     (fun () -> *)
-(*       (\* Png_loader.load_img (Filename "ala"); *\) *)
-(*       let g = new desktop in *)
-(*       let generate_button = new generate_button in *)
-(*       let control_pane = ((new texture_preview generate_button) :> draggable)(\* frame (Layout.fixed_vertical 5 25) *\) in *)
-(*       let open Blocks in *)
-(*       (\* let sl () = new slider (-1.0) 1.0 0.01 in *\) *)
-(*       (\* let sx = (new int_slider 0 5 1.0) in *\) *)
-(*       (\* let sy = sl () in *\) *)
-(*       (\* let sz = sl () in *\) *)
-(*       (\* let sw = sl () in *\) *)
-(*       (\* let graphical_pane = new sphere_view sx sy sz sw in *\) *)
-(*       (\* let control_pane = new frame (Layout.fixed_vertical 5 25) in *\) *)
-(*       let graphical_pane = new block_canvas in *)
-(*       (\* let b1 = (new block "x") in *\) *)
-(*       (\* graphical_pane#add (b1 :> draggable); *\) *)
-(*       (\* b1#invalidate (Rect.rect (300,200) (80,20)); *\) *)
-(*       (\* let b2 = (new block "y") in *\) *)
-(*       (\* graphical_pane#add (b2 :> draggable); *\) *)
-(*       (\* b2#invalidate (Rect.rect (400,200) (80,20)); *\) *)
-(*       (\* let b = (new block "+") in *\) *)
-(*       (\* graphical_pane#add b; *\) *)
-(*       (\* b#invalidate (Rect.rect (400,200) (80,20)); *\) *)
-    
-(*       (\* edit_pane#add (sx :> fixed); *\) *)
-(*       (\* edit_pane#add (sy :> fixed); *\) *)
-(*       (\* edit_pane#add (sz :> fixed); *\) *)
-(*       (\* edit_pane#add (sw :> fixed); *\) *)
-(*       let (_, edit_properties) :: _ = properties in *)
-(*       let edit_pane = new properties edit_properties in *)
-(*       edit_pane#add (generate_button :> fixed); *)
-(*       property_pane # add (edit_pane  :> fixed); *)
-(*       let split_control = (new splitter control_pane (property_pane :> draggable) Vertical) in *)
-(*       let split_display = ((new splitter (split_control :> draggable) (graphical_pane  :> draggable) Horizontal) :> graphical) in *)
-(*       g#add split_display; *)
-(*       let w,h = Display.display_size in *)
-(*       split_display#invalidate (Rect.rect (10,10) (w-20,h-20)); *)
-(*       g#invalidate (Rect.rect (0,0) (w,h)); *)
-(*       ()) *)
 
 let b () =
+  BatArg.handle commandline_spec;
   let open BatInt in
   let g = new desktop in
   let tgv = ((texture_generator_view()) :> graphical) in
   Gui.init
     (fun () ->
       (* Png_loader.load_img (Filename "ala"); *)
-       g#add tgv;
+      g#add tgv;
+      
       ())
     (fun ~width ~height ->
        Window.shelf (Rect.rect (0,0) (width,height));
